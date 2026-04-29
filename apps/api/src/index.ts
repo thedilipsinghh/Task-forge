8913c04ecde6a51e0783d777603a72f08f89c0f2
dilip <dilip@gmail.com>
2026-04-29 09:29:26 +0530

    pg Update

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -3,7 +3,7 @@ import cors from "cors"
 import authRouts from './routes/auth.routes'
 import adminRouts from './routes/admin.routes'
 import empRouts from './routes/employe.routes'
-import { FRONTEND_URL } from "./config/env"
+import { FRONTEND_URL, NODE_ENV, PRODUCTION } from "./config/env"
 import cookieParser from "cookie-parser"
 import { adminProtect, EmployeeProtect } from "./middlewares/auth.middlewares"
 const app = express()
@@ -24,6 +24,10 @@ app.get("/", (req, res) => {
     res.json({ message: "API running successfully" })
 })
 const PORT = 5000
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+if (NODE_ENV! == PRODUCTION) {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`)
+    })
+}
+
+export default app
